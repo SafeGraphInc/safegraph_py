d6dcd5f8ab99f6366d86a1b83d0f73b4474d09e1
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2020-07-22 09:46:08 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-setup(name='safegraph_py_functions',
+setup(name='SafeGraph_py_functions',
 	version='0.1',
     description='people\'s name parser',
     author='lynzt',
     url='https://github.com/SafeGraphInc/safegraph_py',
-    packages = ['safegraph_py_functions']
+    packages = ['SafeGraph_py_functions']
       )
 
