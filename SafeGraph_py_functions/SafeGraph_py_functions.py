2549c8eeaeb6db94dba7e1635cf51b932fdfc809
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2020-07-22 09:39:25 -0400

    Update SafeGraph_py_functions.py

--- SafeGraph_py_functions/SafeGraph_py_functions.py
+++ SafeGraph_py_functions/SafeGraph_py_functions.py
@@ -1,2 +1,39 @@
 def Test_me():
     print("hello world")
+
+
+def help():
+    print('''
+  HELP:
+
+  Welcome to the safegraph helper function. Below you will find a list of functions and their arguments to aid in your datascience journey. If you have further questions that cannot
+  be answered by this help command, please do not hesitate to ask for assistance in the #python_troubleshooting slack channel. 
+
+
+  Available Functions: 
+
+    + Test_me() - A function to test the Python Libray
+    + explode_visitor_home_cbg() - a function to explode visitor home cbg column vertically 
+        **Arguments: 
+          df 
+          json_column 
+          key_column_name 
+          value_column_name 
+          keep_index (bool)** 
+    + explode_json_array() - explodes any generic json array
+        **Arguments:
+          df
+          array_column
+          new_col
+          place_key
+          file_key
+          array_sequence
+          keep_index (bool)**
+          verbose (bool)**
+          zero_index (bool)**
+
+
+
+
+  ''')
+
