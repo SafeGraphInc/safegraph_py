720a6f9b8770f779a514cf859e17e526bad8fba9
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2020-07-24 15:10:52 -0400

    syntax fixes
    fixed most things to run with python3

--- SafeGraph_py_functions/SafeGraph_py_functions.py
+++ SafeGraph_py_functions/SafeGraph_py_functions.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import json
 import os
@@ -14,7 +13,7 @@ def Test_me():
 
 def help():
     print('''
- 
+
  ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- 
 .d8888b.            .d888          .d8888b.                            888           8888888b.           888    888                             888      d8b 888                                       
 d88P  Y88b          d88P"          d88P  Y88b                          888           888   Y88b          888    888                             888      Y8P 888                                       
@@ -37,37 +36,32 @@ Y88b  d88P 888  888 888   Y8b.     Y88b  d88P 888    888  888 888 d88P 888  888 
         Key:
             *    -    Required Argument
             &    -    Boolean value
-            
-            
+
+
 
   Available Functions: 
 
     + Test_me() - A function to test the Python Libray
-  
+
   ----------------------[JSON Section]----------------------
-    
-    + explode_visitor_home_cbg() - a function to explode visitor home cbg column vertically 
+
+    + unpack_json_separate() - a function to explode JSON objects within pandas vertically into a new DF
         **Arguments: 
             df*
             json_column 
             key_column_name 
             value_column_name 
-            keep_index (&)
-            
-    + explode_json_array() - explodes any generic json array | Explodes 'visit_by_day' by default. . . 
+
+    + unpack_json_together() - a function to explode JSON objects within pandas vertically and add it to the current DF 
         **Arguments:
             df*
-            array_column
-            new_col
-            place_key
-            file_key
-            array_sequence
+            json_column
+            key_col
+            value_col
             keep_index (&)
-            verbose (&)
-            zero_index (&)
 
 -----------------[CORE and PATTERNS section]----------------------
-            
+
     + read_core_folder() - a function that concats the core files together into 1 dataframe
         **Arguments:
             path_to_core*
@@ -79,19 +73,19 @@ Y88b  d88P 888  888 888   Y8b.     Y88b  d88P 888    888  888 888 d88P 888  888 
             use_cols
             compression
             nrows
-    
+
     + read_pattern_single() - used to read in SafeGraph data pre June 15th
         **Arguments:
             f_path*
             use_cols
             compression       
-    
+
     + read_pattern_multi() - used to read in SafeGraph pattern data that is broken into multiple files
         **Arguments:
             path_to_pattern*
             use_cols
             compression
-            
+
     + merge_pattern_core() - used to combine the core file and the pattern files on the SafeGraph ID
         **Arguments:
             patterns_df*
@@ -100,10 +94,11 @@ Y88b  d88P 888  888 888   Y8b.     Y88b  d88P 888    888  888 888 d88P 888  888 
 
   ''')
 
+
 ### -------------------------------------- JSON Functions ---------------------------------------------------------------
 
-def vertically_explode_json(df_, json_column='visitor_home_cbgs', key_col_name='visitor_home_cbg',
-                            value_col_name='cbg_visitor_count'):
+def unpack_json_separate(df_, json_column='visitor_home_cbgs', key_col_name='visitor_home_cbg',
+                         value_col_name='cbg_visitor_count'):
     df = df_.copy()
     if (df.index.unique().shape[0] < df.shape[0]):
         raise ("ERROR -- non-unique index found")
@@ -115,63 +110,26 @@ def vertically_explode_json(df_, json_column='visitor_home_cbgs', key_col_name='
         all_sgpid_cbg_data = all_sgpid_cbg_data + this_sgpid_cbg_data
     output = pd.DataFrame(all_sgpid_cbg_data)
     output.set_index('orig_index', inplace=True)
-    return (output)
+    return output
 
-def explode_visitor_home_cbg(df, json_column='visitor_home_cbgs', key_col_name='visitor_home_cbg', value_col_name='cbg_visitor_count', keep_index=False):
-    if(keep_index):
+
+def unpack_json_together(df, json_column='visitor_home_cbgs', key_col_name='visitor_home_cbg',
+                         value_col_name='cbg_visitor_count', keep_index=False):
+    if (keep_index):
         df['index_original'] = df.index
-    df = df.dropna(subset = [json_column]).copy() # Drop nan jsons
-    df.reset_index(drop=True, inplace=True) # Every row must have a unique index
-    df_exp = vertically_explode_json(df, json_column=json_column, key_col_name=key_col_name, value_col_name=value_col_name)
+    df = df.dropna(subset=[json_column]).copy()  # Drop nan jsons
+    df.reset_index(drop=True, inplace=True)  # Every row must have a unique index
+    df_exp = unpack_json_separate(df, json_column=json_column, key_col_name=key_col_name, value_col_name=value_col_name)
     df = df.merge(df_exp, left_index=True, right_index=True).reset_index(drop=True)
-    return(df)
+    return df
 
-
-def explode_json_array(df_, array_column = 'visits_by_day', new_col='day_visit_counts',place_key='safegraph_place_id', file_key='date_range_start', array_sequence='day', keep_index=False, verbose=True, zero_index=False):
-    # This function vertically explodes an array column in SafeGraph data and creates a second new column indicating the index value from the array
-    df = df_.copy()
-    if(verbose): print("Running explode_json_array()")
-    if(keep_index):
-        df['index_original'] = df.index
-    df.reset_index(drop=True, inplace=True) # THIS IS IMPORTANT; explode will not work correctly if index is not unique
-    df[array_column+'_json'] = [json.loads(myjson) for myjson in df[array_column]]
-    day_visits_exp = df[[place_key, file_key, array_column+'_json']].explode(array_column+'_json')
-    day_visits_exp['dummy_key'] = day_visits_exp.index
-    day_visits_exp[array_sequence] = day_visits_exp.groupby([place_key, file_key])['dummy_key'].rank(method='first', ascending=True).astype('int64')
-    if(zero_index):
-      day_visits_exp[array_sequence] = day_visits_exp[array_sequence] -1
-    day_visits_exp.drop(['dummy_key'], axis=1, inplace=True)
-    day_visits_exp.rename(columns={array_column+'_json': new_col}, inplace=True)
-    day_visits_exp[new_col] = day_visits_exp[new_col].astype('int64')
-    df.drop([array_column+'_json'], axis=1, inplace=True)
-    df = pd.merge(df, day_visits_exp, on=[place_key,file_key])
-    return(df)
-
-def unpack_json_key_value_data(df, json_column='visitor_home_cbgs', key_col_name='visitor_home_cbg', value_col_name='cbg_visitor_count', keep_index=False):
-    if(keep_index):
-        df['index_original'] = df.index
-    df = df.dropna(subset = [json_column]).copy() # Drop nan jsons
-    df.reset_index(drop=True, inplace=True) # Every row must have a unique index
-    df_exp = vertically_explode_json(df, json_column=json_column, key_col_name=key_col_name, value_col_name=value_col_name)
-    df = df.merge(df_exp, left_index=True, right_index=True).reset_index(drop=True)
-    return(df)
 
 ### ------------------------------------------ END JSON SECTION--------------------------------------------------------
-### __________________________________________CORE AND PATTERNS SECTION -----------------------------------------------
 
-# Hard coded variable for columns
-
-use_cols_core = ["safegraph_place_id", "location_name", "parent_safegraph_place_id", "safegraph_brand_ids", "brands",
-            "top_category", "sub_category", 'naics_code', "latitude", "longitude", "street_address", "city", "region",
-            "postal_code", "iso_country_code", "phone_number", "open_hours", "category_tags"]
-
-use_cols_pattern = ["safegraph_place_id",	"location_name",	"street_address",	"city",	"region",	"postal_code",	"iso_country_code",
-                    "safegraph_brand_ids",	"brands",	"date_range_start",	"date_range_end",	"raw_visit_counts",	"raw_visitor_counts",
-                    "visits_by_day",	"visits_by_each_hour",	"poi_cbg",	"visitor_home_cbgs",	"visitor_daytime_cbgs",	"visitor_country_of_origin",
-                    "distance_from_home",	"median_dwell",	"bucketed_dwell_times",	"related_same_day_brand",	"related_same_week_brand",	"device_type"]
+### ---------------------------------------CORE AND PATTERNS SECTION -----------------------------------------------
 
 
-def read_core_folder(path_to_core, use_cols=use_cols_core):
+def read_core_folder(path_to_core, use_cols=None):
     core_files = glob.glob(os.path.join(path_to_core, "*.csv.gz"))
     print(f"You are about to load in {len(core_files)} core files")
 
@@ -186,17 +144,20 @@ def read_core_folder(path_to_core, use_cols=use_cols_core):
     return SG_core
 
 
-def read_pattern_demo(f_path, use_cols=use_cols_pattern, compression='gzip', nrows=100):
-    df = pd.read_csv(f_path, dtype={'postal_code': str, 'phone_number': str, 'naics_code': str}, nrows=nrows, compression=compression,
-                     use_cols=use_cols)
+def read_pattern_demo(f_path, use_cols=None, compression='gzip', nrows=100):
+    df = pd.read_csv(f_path, dtype={'postal_code': str, 'phone_number': str, 'naics_code': str}, nrows=nrows,
+                     compression=compression,
+                     usecols=use_cols)
     return df
 
-def read_pattern_single(f_path, use_cols=use_cols_pattern, compression='gzip'):
-  df = pd.read_csv(f_path, dtype={'postal_code': str, 'phone_number': str, 'naics_code': str}, usecols=use_cols, compression=compression)
-  return df
+
+def read_pattern_single(f_path, use_cols=None, compression='gzip'):
+    df = pd.read_csv(f_path, dtype={'postal_code': str, 'phone_number': str, 'naics_code': str}, usecols=use_cols,
+                     compression=compression)
+    return df
 
 
-def read_pattern_multi(path_to_pattern, use_cols=use_cols_pattern, compression='gzip'):
+def read_pattern_multi(path_to_pattern, use_cols=None, compression='gzip'):
     pattern_files = glob.glob(os.path.join(path_to_pattern, "*.csv.gz"))
     print(f"You are about to load in {len(pattern_files)} pattern files")
 
@@ -210,6 +171,7 @@ def read_pattern_multi(path_to_pattern, use_cols=use_cols_pattern, compression='
     SG_pattern = pd.concat(li, axis=0)
     return SG_pattern
 
+
 def merge_pattern_core(patterns_df, core_df, how='left'):
-  merged_df = pd.merge(patterns_df, core_df, on='safegraph_place_id', how=how)
-  return merged_df
+    merged_df = pd.merge(patterns_df, core_df, on='safegraph_place_id', how=how)
+    return merged_df
