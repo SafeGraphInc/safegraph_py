c631151901b0187efe63fa00044b54ee2b52efe2
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2020-08-01 15:48:31 -0400

    snake_case update
    changed folder, file, and setup point

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
-setup(name='SafeGraph_py_functions',
+setup(name='safegraph_py_functions',
 	version='0.1.0',
     description='SafeGraph Python Library',
     author='lynzt',
     url='https://github.com/SafeGraphInc/safegraph_py',
-    packages = ['SafeGraph_py_functions'],
+    packages = ['safegraph_py_functions'],
     python_requires='>=3.6',
       )
 
