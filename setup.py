40ac2488554bba45e8c32f1e1ebc66c54cd0a365
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2020-08-28 18:06:22 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(name='safegraph_py_functions',
-	version=get_version("safegraph_py_functions/__init__.py"),
+	version='1.1.0',
     description='SafeGraph Python Library',
     author='lynzt',
     url='https://github.com/SafeGraphInc/safegraph_py',
     packages = ['safegraph_py_functions'],
     python_requires='>=3.6',
       )
-
