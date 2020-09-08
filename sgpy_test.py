2d6c0767a97b9c2a13953c29436813dd469737ee
Jack <trippl7777@gmail.com>
2020-09-07 19:43:00 -1000

    Update sgpy_test.py

--- sgpy_test.py
+++ sgpy_test.py
@@ -2,12 +2,11 @@
 
 import pytest
 
-def a(x):
-    return x + 1
+class TestClass:
+    def test_one(self):
+        x = "this"
+        assert 'h' in x
 
-
-def test_answer():
-    assert a(3) == 4
-
-def wrong_answer():
-	assert a(2) == 4
+    def test_two(self):
+        x = "hello"
+        assert hasattr(x, 'check')
