b5cdfe58fb3bd249c6a772f71b7687cee23de773
Trippl7777 <47323696+Trippl7777@users.noreply.github.com>
2020-09-23 18:26:55 -1000

    Update read_pattern_test.py

--- tests/read_pattern_test.py
+++ tests/read_pattern_test.py
@@ -11,8 +11,18 @@ import gzip
 
 file_path = "tests/mock_data_v2020_09/weekly_demo.csv.gz"
 
+file_path_multi = "tests/mock_data_v2020_09/weekly_multi"
+
 ### End Expected DFs
 
+
+week1_path = "tests/mock_data_v2020_09/weekly_multi/weekly_demo1.csv.gz"
+week2_path = "tests/mock_data_v2020_09/weekly_multi/weekly_demo2.csv.gz"
+
+week1 = pd.read_csv(week1_path, dtype={'postal_code': str, 'phone_number': str, 'naics_code': str, 'latitude': float, 'longitude': float, 'poi_cbg': str, 'census_block_group': str,'primary_number': str}, compression='gzip')
+week2 = pd.read_csv(week2_path, dtype={'postal_code': str, 'phone_number': str, 'naics_code': str, 'latitude': float, 'longitude': float, 'poi_cbg': str, 'census_block_group': str,'primary_number': str}, compression='gzip')
+
+# combined = pd.concat([week1, week2], ignore_index=True)
 
 ### Test section
 
@@ -36,14 +46,14 @@ def test_read_pattern_single():
 
     pdt.assert_frame_equal(action, expected)
 
-# def test_unpack_json_fast():
-#     ''' This is a test of unpack json fast '''
+def test_read_pattern_multi():
+    ''' This is a test of unpack json fast '''
 
-#     action1 = unpack_json_fast(df)
+    action1 = read_pattern_multi(file_path_multi)
 
-#     expected1 = pd.DataFrame(expected_data, index=new_index).rename_axis('orig_index')
-
-#     pdt.assert_frame_equal(action1, expected1)
+    expected1 = pd.concat([week1, week2], ignore_index=True)
+    
+    pdt.assert_frame_equal(action1, expected1)
 
 ### |-------------- Only uncomment when you need to test pytest FAIL functionality -------------|
 
